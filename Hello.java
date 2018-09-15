6c639140aa2ebd463aaad180041661493f7364a9
RoboticsRafsan01 <mzaman1@stuy.edu>
2018-09-14 21:22:37 -0400

    Now its complete

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello{
   public status void main(String[] args) {
-    System.out.println("this is only a start")
+    System.out.println("Hello my name is Md. Rafsan Zaman")
+	System.out.println("Have a nice day")
     }
     }
     
