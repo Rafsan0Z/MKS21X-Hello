a828447458e97a7cad16036b9ede999926479fad
RoboticsRafsan01 <mzaman1@stuy.edu>
2018-09-16 19:39:17 -0400

    final change

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
-  public status void main(String[] args) {
-    System.out.println("Hello my name is Md. Rafsan Zaman")
-	System.out.println("Have a nice day")
+  public static void main(String[] args) {
+    System.out.println("Hello my name is Md. Rafsan Zaman");
+    System.out.println("Have a nice day");
     }
     }
     
